5a9d0a76afbe10fe3455204dceb8e1387c3db629
Chad Schwab, Roger Landreth <roger.landreth@asynchrony.com>
2017-04-18 11:27:43 -0500

    577 Opportunity creation now done through single message to Sparkbot

--- src/skills/opportunity_create.js
+++ src/skills/opportunity_create.js
@@ -1,31 +1,18 @@
 const opportunityCreate = (controller, jsforceConn) => {
-  const goToNextQuestion = (response, convo) => convo.next();
-  const createOpportunity = (response, convo) => {
+  controller.hears(['opp create <(.*)> <(.*)> <(.*)>'], 'direct_message', (bot, message) => {
     jsforceConn.sobject('Opportunity').create(
       {
-        Name: convo.responses.oppName.text,
-        StageName: convo.responses.stageName.text,
-        CloseDate: convo.responses.closeDate.text,
+        Name: message.match[1],
+        StageName: message.match[2],
+        CloseDate: message.match[3],
       },
       (error, result) => {
         if (error) {
-          convo.say(`Error: ${error}`);
+          bot.reply(message, `Error: ${error}`);
         } else {
-          convo.say(`Success: [${convo.responses.oppName.text}](${process.env.base_url}${result.id})`);
+          bot.reply(message, `Success: [${message.match[1]}](${process.env.base_url}${result.id})`);
         }
-        convo.next();
       });
-  };
-  controller.hears(['opp create'], 'direct_message', (bot, message) => {
-    bot.startConversation(message, (error, convo) => {
-      if (error) {
-        bot.say('Error starting conversation');
-      } else {
-        convo.addQuestion('What is the opportunity name?', goToNextQuestion, { key: 'oppName' });
-        convo.addQuestion('What stage is the opportunity in?', goToNextQuestion, { key: 'stageName' });
-        convo.addQuestion('What is the close date?', createOpportunity, { key: 'closeDate' });
-      }
-    });
   });
 };
 
