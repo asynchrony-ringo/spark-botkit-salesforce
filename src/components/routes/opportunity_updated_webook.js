1d5923afff6159487097e933c5226f178232e26c
Alex Agatstein, Chad Schwab <chad.schwab@asynchrony.com>
2017-04-20 13:49:05 -0500

    Finishing up opportunity updated webhook

--- src/components/routes/opportunity_updated_webook.js
+++ src/components/routes/opportunity_updated_webook.js
@@ -1,30 +1,42 @@
 const debug = require('debug')('botkit:incoming_webhooks');
 
-module.exports = (webserver, controller, jsforceConn) => {
+const validateRequest = (req, res) => {
+  if (!req.body.attributes || !req.body.attributes.type || req.body.attributes.type !== 'Opportunity') {
+    res.status(400);
+    res.send('Bad Request');
+    return false;
+  }
+  res.status(200);
+  res.send('ok');
+  return true;
+};
+
+const opportunityUpdatedWebhook = (webserver, controller, jsforceConn) => {
   debug('Configured POST /salesforce/update for receiving events');
   webserver.post('/salesforce/update', (req, res) => {
-    // NOTE: we should enforce the token check here
-    // respond to spark that the webhook has been received.
+    if (!validateRequest(req, res)) { return; }
 
-    if (!req.body.attributes || !req.body.attributes.type || req.body.attributes.type !== 'Opportunity') {
-      res.status(400);
-      res.send('Bad Request');
-      return;
-    }
-
-    res.status(200);
-    res.send('ok');
-
-    jsforceConn.sobject('User').retrieve(req.body.OwnerId, (error, user) => {
-      if (!error) {
+    if (req.body.OwnerId) {
+      jsforceConn.sobject('User').retrieve(req.body.OwnerId, (userRetrievalError, user) => {
+        if (userRetrievalError) {
+          console.log(`Error retrieving user ${req.body.OwnerId}:`, userRetrievalError);
+          return;
+        }
         const bot = controller.spawn({});
 
-        bot.startPrivateConversation({ user: user.Email }, (err, conversation) => {
-          if (err) { console.log('Error: ', err); return; }
+        bot.startPrivateConversation({ user: user.Email },
+          (startConversationError, conversation) => {
+            if (startConversationError) {
+              console.log(`Error starting conversation with ${user.Email}:`, startConversationError);
+              return;
+            }
 
-          conversation.say(`An opportunity you own has been updated! ${process.env.base_url}${req.body.Id}`);
-        });
-      }
-    });
+            conversation.say(`An opportunity you own has been updated! [${req.body.Name}](${process.env.base_url}${req.body.Id})`);
+          });
+      });
+    }
   });
 };
+
+
+module.exports = opportunityUpdatedWebhook;
--- test/components/routes/opportunity_updated_webhook_tests.js
+++ test/components/routes/opportunity_updated_webhook_tests.js
@@ -5,6 +5,7 @@ const expect = require('chai').expect;
 describe('incoming web hook for opportunity update', () => {
   let webserver;
   let jsforceConn;
+  let controller;
 
   beforeEach(() => {
     webserver = {
@@ -13,7 +14,9 @@ describe('incoming web hook for opportunity update', () => {
 
     jsforceConn = {
     };
-    testModule(webserver, null, jsforceConn);
+    controller = {
+    };
+    testModule(webserver, controller, jsforceConn);
   });
 
 
@@ -30,44 +33,135 @@ describe('incoming web hook for opportunity update', () => {
   describe('webserver post callback', () => {
     let webserverPostCallback;
     let retrieveStub;
+    let request;
+    let response;
+
     beforeEach(() => {
       webserverPostCallback = webserver.post.args[0][1];
       retrieveStub = sinon.stub();
       jsforceConn.sobject = sinon.stub();
       jsforceConn.sobject.returns({ retrieve: retrieveStub });
-    });
 
-    it('should return a successful status and message when the request is of correct type', () => {
-      const res = {
+      response = {
         status: sinon.stub(),
         send: sinon.stub(),
       };
 
-      webserverPostCallback({
-        body: {
-          attributes: {
-            type: 'Opportunity',
-          },
-        },
-      }, res);
+      request = { body: {} };
+    });
 
-      expect(res.status.calledWith(200)).to.be.true;
-      expect(res.send.calledWith('ok')).to.be.true;
+    it('should return a successful status and message when the request is of correct type', () => {
+      request.body = {
+        attributes: {
+          type: 'Opportunity',
+        },
+      };
+      webserverPostCallback(request, response);
+
+      expect(response.status.calledWith(200)).to.be.true;
+      expect(response.send.calledWith('ok')).to.be.true;
     });
 
     [{ attributes: { type: 'Foo' } }, { attributes: {} }, {}].forEach((requestBody) => {
       it(`should return a failing status and message when the request is ${JSON.stringify(requestBody)}`, () => {
-        const res = {
-          status: sinon.stub(),
-          send: sinon.stub(),
+        request.body = requestBody;
+        webserverPostCallback(request, response);
+
+        expect(response.status.calledWith(400)).to.be.true;
+        expect(response.send.calledWith('Bad Request')).to.be.true;
+      });
+    });
+
+    it('should not retrieve a user if there is no owner id', () => {
+      request.body = {
+        attributes: {
+          type: 'Opportunity',
+        },
+        ownerId: null,
+      };
+      webserverPostCallback(request, response);
+      expect(jsforceConn.sobject.notCalled).to.be.true;
+    });
+
+    describe('when ownder id exists', () => {
+      beforeEach(() => {
+        request.body = {
+          attributes: {
+            type: 'Opportunity',
+          },
+          OwnerId: 1234,
         };
+        webserverPostCallback(request, response);
+      });
 
-        webserverPostCallback({
-          body: requestBody,
-        }, res);
+      it('should create sobject for User query', () => {
+        expect(jsforceConn.sobject.calledOnce).to.be.true;
+        expect(jsforceConn.sobject.args[0][0]).to.equal('User');
+      });
 
-        expect(res.status.calledWith(400)).to.be.true;
-        expect(res.send.calledWith('Bad Request')).to.be.true;
+      it('should retrieve user based off ownderId', () => {
+        expect(retrieveStub.calledOnce).to.be.true;
+        expect(retrieveStub.args[0][0]).to.equal(1234);
+        expect(retrieveStub.args[0][1]).to.be.a('Function');
+      });
+
+      describe('retrieve callback', () => {
+        let retrieveCallback;
+        let bot;
+
+        beforeEach(() => {
+          retrieveCallback = retrieveStub.args[0][1];
+          bot = {
+            startPrivateConversation: sinon.stub(),
+          };
+          controller.spawn = sinon.stub().returns(bot);
+        });
+
+        it('should not start a conversation if there is an error', () => {
+          retrieveCallback(true, {});
+          expect(controller.spawn.notCalled).to.be.true;
+        });
+
+        it('should spawn a bot when no error', () => {
+          retrieveCallback(null, {});
+          expect(controller.spawn.calledOnce).to.be.true;
+        });
+
+        it('should start a conversation with the returned user', () => {
+          retrieveCallback(null, { Email: 'some.email@some-domain.ext' });
+          expect(bot.startPrivateConversation.calledOnce).to.be.true;
+          expect(bot.startPrivateConversation.args[0][0]).to.deep.equal({ user: 'some.email@some-domain.ext' });
+          expect(bot.startPrivateConversation.args[0][1]).to.be.a('Function');
+        });
+
+        describe('conversation callback', () => {
+          let conversationCallback;
+          let conversation;
+
+          beforeEach(() => {
+            process.env.base_url = 'niceurl.some-domain.ext/';
+            retrieveCallback(null, { Email: 'some.email@some-domain.ext' });
+            conversationCallback = bot.startPrivateConversation.args[0][1];
+            conversation = { say: sinon.stub() };
+          });
+
+          afterEach(() => {
+            delete process.env.base_url;
+          });
+
+          it('should not say anything if error', () => {
+            conversationCallback(true, conversation);
+            expect(conversation.say.notCalled).to.be.true;
+          });
+
+          it('should tell the user an opportunity has been created if success', () => {
+            request.body.Name = 'really_nice_name';
+            request.body.Id = 'even_better_id';
+            conversationCallback(null, conversation);
+            expect(conversation.say.called).to.be.true;
+            expect(conversation.say.args[0][0]).to.equal('An opportunity you own has been updated! [really_nice_name](niceurl.some-domain.ext/even_better_id)');
+          });
+        });
       });
     });
   });
