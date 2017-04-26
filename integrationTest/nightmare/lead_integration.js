123285662f04b01bfdc1505b8b75cd385e878cf5
Alex Agatstein, Roger Landreth <roger.landreth@asynchrony.com>
2017-04-26 14:53:33 -0500

    589 - added integration test

---
+++ integrationTest/nightmare/lead_integration.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect;
+const Nightmare = require('nightmare');
+const env = require('node-env-file');
+const nightmareHelpers = require('./nightmare_helpers.js');
+
+const leadId = '00Q18000002t4w8';
+env('.env');
+
+describe('lead', () => {
+  it.only('should respond with lead status after query in a direct message and group message', () => {
+    const nightmare = Nightmare({ show: true, waitTimeout: 60000 });
+    return nightmare
+      .use(nightmareHelpers.login)
+      .use(nightmareHelpers.startPrivateConversation)
+      .use(nightmareHelpers.sendMessage(`lead status ${leadId}`))
+      .use(nightmareHelpers.evaluateNextSFBotResponse)
+      .then((directMessageStatusResponse) => {
+        const expectedStatusMatch = new RegExp(`Information for lead: [${leadId}]`);
+        expect(directMessageStatusResponse).to.match(expectedStatusMatch);
+        return nightmare
+          .use(nightmareHelpers.goHome)
+          .use(nightmareHelpers.startGroupConversation)
+          .use(nightmareHelpers.sendMentionMessage(`lead status ${leadId}`))
+          .use(nightmareHelpers.evaluateNextSFBotResponse)
+          .end()
+          .then((directMentionStatusResponse) => {
+            expect(directMentionStatusResponse).to.match(expectedStatusMatch);
+          });
+      });
+  });
+});
