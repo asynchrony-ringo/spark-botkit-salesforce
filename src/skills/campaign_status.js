f03b50e5f362761a3bbe8678a294e605b3f72a35
Alex Agatstein, Roger Landreth <roger.landreth@asynchrony.com>
2017-04-26 14:37:13 -0500

    589 - campaign status skill now uses the status controller for jsforce query / bot response

--- src/skills/campaign_status.js
+++ src/skills/campaign_status.js
@@ -1,19 +1,8 @@
+const statusController = require('../skillsControllers/status_controller.js');
+
 const campaignStatus = (controller, jsforceConn) => {
   controller.hears(['campaign status (.*)'], 'direct_message,direct_mention', (bot, message) => {
-    const campaignId = message.match[1];
-    jsforceConn.sobject('Campaign').retrieve(campaignId, (error, campaign) => {
-      if (!error) {
-        const campaignResponse = `Information for campaign: [${campaignId}](${process.env.base_url}${campaignId})\n` +
-          `* Name: ${campaign.Name}\n` +
-          `* Description: ${campaign.Description}\n` +
-          `* Start Date: ${campaign.StartDate}\n` +
-          `* End Date: ${campaign.EndDate}\n`;
-
-        bot.reply(message, campaignResponse);
-      } else {
-        bot.reply(message, `Sorry, I was unable to retrieve your campaign: ${campaignId}. ${error}`);
-      }
-    });
+    statusController.replyWithStatus('Campaign', message.match[1], ['Name', 'Description', 'StartDate', 'EndDate'], bot, message, jsforceConn);
   });
 };
 
--- test/skills/campaign_status_tests.js
+++ test/skills/campaign_status_tests.js
@@ -1,6 +1,7 @@
 const sinon = require('sinon');
 const expect = require('chai').expect;
 const campaignStatus = require('../../src/skills/campaign_status.js');
+const statusController = require('../../src/skillsControllers/status_controller.js');
 
 describe('campaign status', () => {
   let controller;
@@ -20,83 +21,25 @@ describe('campaign status', () => {
   });
 
   describe('listener callback', () => {
-    const campaignId = 'campaignId';
+    let listenerCallback;
     let bot;
     let message;
-    let error = null;
-    let campaign = { };
-    let listenerCallback;
-    let sobject;
-    let retrieve;
 
     beforeEach(() => {
       bot = { reply: sinon.spy() };
-
-      message = { match: [null, campaignId] };
-
-      retrieve = sinon.spy((id, callback) => {
-        callback(error, campaign);
-      });
-      sobject = sinon.spy(() => ({ retrieve }));
-      jsforceConn.sobject = sobject;
-
+      message = { match: [null, 'campaignId'] };
+      sinon.stub(statusController, 'replyWithStatus');
       listenerCallback = controller.hears.args[0][2];
     });
 
-    it('calls jsforce connection\'s sobject method', () => {
+    afterEach(() => {
+      statusController.replyWithStatus.restore();
+    });
+
+    it('should call status controller\'s replyWithStatus method', () => {
       listenerCallback(bot, message);
-      expect(sobject.calledOnce).to.be.true;
-      expect(sobject.args[0][0]).to.equal('Campaign');
-    });
-
-    it('calls jsforce connection\'s retrieve method', () => {
-      listenerCallback(bot, message);
-      expect(retrieve.calledOnce).to.be.true;
-      expect(retrieve.args[0][0]).to.deep.equal(campaignId);
-    });
-
-    describe('when given a valid campaign id', () => {
-      beforeEach(() => {
-        campaign = {
-          Name: 'Test Campaign',
-          Description: 'Test Description',
-          StartDate: '2017-04-24',
-          EndDate: '2017-05-24',
-        };
-
-        process.env.base_url = 'awesomesauce.com/';
-        listenerCallback(bot, message);
-      });
-
-      afterEach(() => {
-        delete process.env.base_url;
-      });
-
-      it('should reply with the campaign details', () => {
-        expect(bot.reply.calledOnce).to.be.true;
-        expect(bot.reply.args[0][0]).to.equal(message);
-        const responseMessage = bot.reply.args[0][1];
-        const messageParts = responseMessage.split('*');
-        expect(messageParts.length).to.equal(5);
-        expect(messageParts[0]).to.equal(`Information for campaign: [${campaignId}](awesomesauce.com/${campaignId})\n`);
-        expect(messageParts[1]).to.equal(` Name: ${campaign.Name}\n`);
-        expect(messageParts[2]).to.equal(` Description: ${campaign.Description}\n`);
-        expect(messageParts[3]).to.equal(` Start Date: ${campaign.StartDate}\n`);
-        expect(messageParts[4]).to.equal(` End Date: ${campaign.EndDate}\n`);
-      });
-    });
-
-    describe('when given an invalid campaign id', () => {
-      beforeEach(() => {
-        error = 'Error: tribbles!!';
-
-        listenerCallback(bot, message);
-      });
-      it('should reply with an error message', () => {
-        expect(bot.reply.calledOnce).to.be.true;
-        expect(bot.reply.args[0][0]).to.equal(message);
-        expect(bot.reply.args[0][1]).to.equal(`Sorry, I was unable to retrieve your campaign: ${campaignId}. ${error}`);
-      });
+      expect(statusController.replyWithStatus.calledOnce).to.be.true;
+      expect(statusController.replyWithStatus.calledWith('Campaign', message.match[1], ['Name', 'Description', 'StartDate', 'EndDate'], bot, message, jsforceConn)).to.be.true;
     });
   });
 });
