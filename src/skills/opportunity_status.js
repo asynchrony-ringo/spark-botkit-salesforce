70c76f7bbc28166a7ac99a83b66f6a5882e2985c
Alex Agatstein, Roger Landreth <roger.landreth@asynchrony.com>
2017-04-26 14:46:15 -0500

    589 - opportunity status skill now uses the status controller for jsforce query / bot response

--- src/skills/opportunity_status.js
+++ src/skills/opportunity_status.js
@@ -1,18 +1,8 @@
+const statusController = require('../skillsControllers/status_controller.js');
+
 const opportunityStatus = (controller, jsforceConn) => {
   controller.hears(['opp status (.*)'], 'direct_message,direct_mention', (bot, message) => {
-    const oppId = message.match[1];
-    jsforceConn.sobject('Opportunity').retrieve(oppId, (error, opp) => {
-      if (!error) {
-        const oppResponse = `Information for opportunity: [${oppId}](${process.env.base_url}${oppId})\n` +
-          `* Name: ${opp.Name}\n` +
-          `* Current stage: ${opp.StageName}\n` +
-          `* Close Date: ${opp.CloseDate}\n`;
-
-        bot.reply(message, oppResponse);
-      } else {
-        bot.reply(message, `Sorry, I was unable to retrieve your opportunity: ${oppId}. ${error}`);
-      }
-    });
+    statusController.replyWithStatus('Opportunity', message.match[1], ['Name', 'StageName', 'CloseDate'], bot, message, jsforceConn);
   });
 };
 
--- test/skills/opportunity_status_tests.js
+++ test/skills/opportunity_status_tests.js
@@ -1,19 +1,15 @@
 const sinon = require('sinon');
 const expect = require('chai').expect;
 const opportunityStatus = require('../../src/skills/opportunity_status.js');
+const statusController = require('../../src/skillsControllers/status_controller.js');
 
 describe('opportunity status', () => {
-  const controller = { hears: sinon.spy() };
-  const oppId = 'literallyAnything';
-  let error = null;
-  let opp = {};
-  const retrieve = sinon.spy((id, callback) => {
-    callback(error, opp);
-  });
-  const sobject = sinon.spy(() => ({ retrieve }));
-  const jsforceConn = { sobject };
+  let controller;
+  let jsforceConn;
 
   beforeEach(() => {
+    controller = { hears: sinon.spy() };
+    jsforceConn = {};
     opportunityStatus(controller, jsforceConn);
   });
 
@@ -25,65 +21,25 @@ describe('opportunity status', () => {
   });
 
   describe('listener callback', () => {
+    let listenerCallback;
     let bot;
     let message;
-    let listenerCallback;
 
     beforeEach(() => {
       bot = { reply: sinon.spy() };
-      message = { match: [null, oppId] };
+      message = { match: [null, 'opportunityId'] };
+      sinon.stub(statusController, 'replyWithStatus');
       listenerCallback = controller.hears.args[0][2];
     });
 
-    it('calls jsforce connection\'s sobject retrieve method', () => {
+    afterEach(() => {
+      statusController.replyWithStatus.restore();
+    });
+
+    it('should call status controller\'s replyWithStatus method', () => {
       listenerCallback(bot, message);
-      expect(sobject.calledOnce).to.be.true;
-      expect(sobject.args[0][0]).to.equal('Opportunity');
-      expect(retrieve.calledOnce).to.be.true;
-      expect(retrieve.args[0][0]).to.equal(oppId);
-      expect(retrieve.args[0][1]).to.be.a('function');
-    });
-
-    describe('when given a valid opportunity id', () => {
-      beforeEach(() => {
-        opp = {
-          Name: 'Super Awesome Opportunity',
-          StageName: 'Research',
-          CloseDate: '11/13/1988',
-        };
-
-        process.env.base_url = 'salesforce.instance/';
-        listenerCallback(bot, message);
-      });
-
-      afterEach(() => {
-        delete process.env.base_url;
-      });
-
-      it('should reply with the opportunity details', () => {
-        expect(bot.reply.calledOnce).to.be.true;
-        expect(bot.reply.args[0][0]).to.equal(message);
-        const responseMessage = bot.reply.args[0][1];
-        const messageParts = responseMessage.split('*');
-        expect(messageParts.length).to.equal(4);
-        expect(messageParts[0]).to.equal('Information for opportunity: [literallyAnything](salesforce.instance/literallyAnything)\n');
-        expect(messageParts[1]).to.equal(' Name: Super Awesome Opportunity\n');
-        expect(messageParts[2]).to.equal(' Current stage: Research\n');
-        expect(messageParts[3]).to.equal(' Close Date: 11/13/1988\n');
-      });
-    });
-
-    describe('when given an invalid opportunity id', () => {
-      beforeEach(() => {
-        error = 'Error: tribbles!!';
-
-        listenerCallback(bot, message);
-      });
-      it('should reply with an error message', () => {
-        expect(bot.reply.calledOnce).to.be.true;
-        expect(bot.reply.args[0][0]).to.equal(message);
-        expect(bot.reply.args[0][1]).to.equal('Sorry, I was unable to retrieve your opportunity: literallyAnything. Error: tribbles!!');
-      });
+      expect(statusController.replyWithStatus.calledOnce).to.be.true;
+      expect(statusController.replyWithStatus.calledWith('Opportunity', message.match[1], ['Name', 'StageName', 'CloseDate'], bot, message, jsforceConn)).to.be.true;
     });
   });
 });
