d3ce610515d804cd30d4964ef4390b81c974c073
Kara Kimmel, Chad Schwab <chad.schwab@asynchrony.com>
2017-05-01 10:21:57 -0500

    629 - adding working hmacSHA1 check for cisco requests

--- src/components/express_webserver.js
+++ src/components/express_webserver.js
@@ -1,32 +1,31 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var querystring = require('querystring');
-var debug = require('debug')('botkit:webserver');
-
-module.exports = function(controller, jsforceConn) {
-
-
-    var webserver = express();
-    webserver.use(bodyParser.json());
-    webserver.use(bodyParser.urlencoded({ extended: true }));
-
-    webserver.use(express.static('public'));
+const express = require('express');
+const bodyParser = require('body-parser');
+const debug = require('debug')('botkit:webserver');
+const hmacSHA1 = require('crypto-js/hmac-sha1');
+const utf8 = require('crypto-js/enc-utf8');
 
 
-    webserver.listen(process.env.PORT || 3000, null, function() {
+module.exports = function (controller, jsforceConn) {
+  const webserver = express();
 
-        debug('Express webserver configured and listening at http://localhost:' + process.env.PORT || 3000);
+  webserver.use(bodyParser.json({ verify: (req, res, buff, encoding) => {
+    req.hmacSHA1 = hmacSHA1(buff.toString(encoding), process.env.secret).toString();
+  },
+  }));
+  webserver.use(bodyParser.urlencoded({ extended: true }));
 
-    });
+  webserver.use(express.static('public'));
+  webserver.listen(process.env.PORT || 3000, null, () => {
+    debug(`Express webserver configured and listening at http://localhost:${process.env.PORT}` || 3000);
+  });
 
     // import all the pre-defined routes that are present in /components/routes
-    var normalizedPath = require("path").join(__dirname, "routes");
-    require("fs").readdirSync(normalizedPath).forEach(function(file) {
-      require("./routes/" + file)(webserver, controller, jsforceConn);
-    });
+  const normalizedPath = require('path').join(__dirname, 'routes');
+  require('fs').readdirSync(normalizedPath).forEach((file) => {
+    require(`./routes/${file}`)(webserver, controller, jsforceConn);
+  });
 
-    controller.webserver = webserver;
+  controller.webserver = webserver;
 
-    return webserver;
-
-}
+  return webserver;
+};
--- src/components/routes/incoming_webhooks.js
+++ src/components/routes/incoming_webhooks.js
@@ -2,16 +2,17 @@ const debug = require('debug')('botkit:incoming_webhooks');
 
 module.exports = function (webserver, controller) {
   debug('Configured POST /ciscospark/receive url for receiving events');
-  webserver.post('/ciscospark/receive', (req, res) => {
-        // NOTE: we should enforce the token check here
 
-        // respond to Slack that the webhook has been received.
-    res.status(200);
-    res.send('ok');
+  webserver.post('/ciscospark/receive', (req, res) => {
+    if (req.hmacSHA1 !== req.get('X-Spark-Signature')) {
+      res.status(403).send(`HMAC validation error\n${req.hmacSHA1}${typeof req.hmacSHA1}\n${req.get('X-Spark-Signature')}${typeof req.get('X-Spark-Signature')}`);
+      return;
+    }
+    res.status(200).send('ok');
 
     const bot = controller.spawn({});
 
-        // Now, pass the webhook into be processed
+    // Now, pass the webhook into be processed
     controller.handleWebhookPayload(req, res, bot);
   });
 };
