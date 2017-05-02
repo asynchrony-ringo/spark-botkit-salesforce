d6e3d8ac5e32d62f2194c3468d626445682424f3
Chad Schwab, Alex Agatstein <alex.agatstein@asynchrony.com>
2017-05-02 15:31:45 -0500

    633 - code clean up

--- src/components/express_webserver.js
+++ src/components/express_webserver.js
@@ -1,16 +1,9 @@
+/* eslint-disable */
 const express = require('express');
-const bodyParser = require('body-parser');
 const debug = require('debug')('botkit:webserver');
-const hmacSHA1 = require('crypto-js/hmac-sha1');
 
 module.exports = function (controller, jsforceConn) {
   const webserver = express();
-
-  webserver.use(bodyParser.json({ verify: (req, res, buff, encoding) => {
-    req.hmacSHA1 = hmacSHA1(buff.toString(encoding), process.env.secret).toString();
-  },
-  }));
-  webserver.use(bodyParser.urlencoded({ extended: true }));
 
   webserver.use(express.static('public'));
   webserver.listen(process.env.PORT || 3000, null, () => {
