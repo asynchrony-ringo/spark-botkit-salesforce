e97e31083d87104a6405853b2141fa52b4f52bad
Chad Schwab, Alex Agatstein <alex.agatstein@asynchrony.com>
2017-05-02 15:07:58 -0500

    632 - Ignore leading/trailing white space on all commands that ask for an Id

--- test/skills/opportunity_status_tests.js
+++ test/skills/opportunity_status_tests.js
@@ -20,7 +20,7 @@ describe('opportunity status', () => {
 
   it('should register hear listener on controller', () => {
     expect(controller.hears.calledOnce).to.be.true;
-    expect(controller.hears.args[0][0]).to.deep.equal(['^opp status (.*)$']);
+    expect(controller.hears.args[0][0]).to.deep.equal(['^opp status[ ]+(.*)[ ]*$']);
     expect(controller.hears.args[0][1]).to.equal('direct_message,direct_mention');
     expect(controller.hears.args[0][2]).to.be.a('function');
   });
