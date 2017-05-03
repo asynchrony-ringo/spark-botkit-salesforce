4361b52478e456941187bcba4c221216ce19d821
Kara Kimmel, Alex Agatstein <alex.agatstein@asynchrony.com>
2017-05-03 16:41:04 -0500

    638 - Long values or values with newlines now use markdown blockquotes in the difference gatherer

--- src/components/routeControllers/update_alert_difference_gatherer.js
+++ src/components/routeControllers/update_alert_difference_gatherer.js
@@ -1,6 +1,14 @@
 const fieldsToIgnore = ['CompareName'];
+const maxInlineLength = 150;
 
 const doesNotExist = (obj, key) => !obj.hasOwnProperty(key) || obj[key] === '' || obj[key] === null;
+
+const formatValue = (value) => {
+  if (typeof value === 'string' && (value.length > maxInlineLength || value.match(/[\n\r]+/))) {
+    return `\n> ${value.split(/[\n\r]+/).join(' ')}\n\n`;
+  }
+  return `**${value}**`;
+};
 
 const updateAlertDifferenceGatherer = {
   formatMessage: (newObject = {}, oldObject = {}) => {
@@ -17,9 +25,9 @@ const updateAlertDifferenceGatherer = {
         if (doesNotExist(newObject, key) && !doesNotExist(oldObject, key)) {
           diffs.push(` * ${key} was removed`);
         } else if (doesNotExist(oldObject, key) && !doesNotExist(newObject, key)) {
-          diffs.push(` * ${key} was added: ${newObject[key]}`);
+          diffs.push(` * ${key} was added: ${formatValue(newObject[key])}`);
         } else if (newObject[key] !== oldObject[key]) {
-          diffs.push(` * ${key} was updated from ${oldObject[key]} to ${newObject[key]}`);
+          diffs.push(` * ${key} was updated from ${formatValue(oldObject[key])} to ${formatValue(newObject[key])}`);
         }
       });
 
